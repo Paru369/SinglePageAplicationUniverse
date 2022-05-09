cbcc07c1909b8f8184019231b8dbc67020edd2f4
Paru - Paulo Victor Pinheiro <99094718+Paru369@users.noreply.github.com>
2022-05-08 21:39:20 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ import { Router }  from './router.js'
 
 const router = new Router()
 
-router.add( '/', "/pages/home.html")
-router.add( '/index.html', "/pages/home.html")
-router.add("/about", "/pages/about.html" )
-router.add("/exploration", "/pages/exploration.html" )
-router.add( 404, "/pages/404.html")
+router.add( '/', "./pages/home.html")
+router.add( '/index.html', "./pages/home.html")
+router.add("/about", "./pages/about.html" )
+router.add("/exploration", "./pages/exploration.html" )
+router.add( 404, "./pages/404.html")
 
 
 
